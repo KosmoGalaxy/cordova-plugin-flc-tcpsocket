312f64781cc7f6f757ad824c888a11cab30d4f6a
gosuhiman <gosuhiman@gmail.com>
2018-04-16 07:44:44 -0700

    1.1.0 iOS

---
+++ src/ios/pl/fulllegitcode/tcpsocket/FlcTcpSocketPlugin.swift
@@ -0,0 +1,20 @@
+//
+//  FlcTcpSocketPlugin.swift
+//  HelloCordova
+//
+//  Created by wojcieszki on 11/04/2018.
+//
+
+import Foundation
+
+@objc(FlcTcpSocketPlugin) class FlcTcpSocketPlugin : CDVPlugin {
+  
+  override init() {
+    super.init()
+  }
+  
+  override func pluginInitialize() {
+    super.pluginInitialize()
+  }
+  
+}
